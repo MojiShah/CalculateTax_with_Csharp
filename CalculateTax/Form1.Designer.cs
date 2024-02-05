d3cc9088e156f8507cb1a217f01245665bd3cd11
Mojtaba Charchian <mojishah70@gmail.com>
2024-02-05 18:07:24 +0330

    calculate Tax project

---
+++ CalculateTax/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace CalculateTax
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterYourName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.lblEnterYourSalary = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterYourName
+            // 
+            this.lblEnterYourName.AutoSize = true;
+            this.lblEnterYourName.Location = new System.Drawing.Point(38, 44);
+            this.lblEnterYourName.Name = "lblEnterYourName";
+            this.lblEnterYourName.Size = new System.Drawing.Size(160, 24);
+            this.lblEnterYourName.TabIndex = 5;
+            this.lblEnterYourName.Text = "نام را وارد  کنید:";
+            this.lblEnterYourName.Click += new System.EventHandler(this.lblEnterYourName_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(229, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(270, 32);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(229, 92);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(270, 32);
+            this.txtSalary.TabIndex = 1;
+            this.txtSalary.TextChanged += new System.EventHandler(this.txtSalary_TextChanged);
+            // 
+            // lblEnterYourSalary
+            // 
+            this.lblEnterYourSalary.AutoSize = true;
+            this.lblEnterYourSalary.Location = new System.Drawing.Point(38, 95);
+            this.lblEnterYourSalary.Name = "lblEnterYourSalary";
+            this.lblEnterYourSalary.Size = new System.Drawing.Size(183, 24);
+            this.lblEnterYourSalary.TabIndex = 6;
+            this.lblEnterYourSalary.Text = "حقوق را وارد کنید:";
+            this.lblEnterYourSalary.Click += new System.EventHandler(this.lblEnterYourSalary_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 157);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(171, 49);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "محاسبه مالیات";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(198, 157);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(141, 49);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "پاک کردن";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(345, 157);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(141, 49);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "خروج";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(38, 222);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(61, 24);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "نتیجه";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 297);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblEnterYourSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblEnterYourName);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "Calculate Tax";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterYourName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label lblEnterYourSalary;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ CalculateTax/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateTax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double Tax, salary, psalary;
+            salary=Convert.ToDouble(txtSalary.Text);
+            if (salary < 500000)
+                Tax = 0;
+            else if (salary < 1000000)
+                Tax = 0.05 * salary;
+            else if (salary < 2000000)
+                Tax = 0.1 * salary;
+            else if (salary < 3000000)
+                Tax = 0.15 * salary;
+            else 
+                Tax = 0.2 * salary;
+
+            psalary = salary - Tax;
+            lblResult.Text = txtName.Text + " مالیات شما هست: " + Tax.ToString() + 
+                "تومان" + "\nحقوق خالص شما برابر است با: : " + psalary.ToString() + "تومان";
+            //string s;
+            //s = txtName.Text + " مالیات شما هست: " + Tax.ToString() + "تومان" + "\nحقوق خالص شما برابر است با: : " + psalary.ToString() + "تومان";
+            //MessageBox.Show(s);
+            //txtName.Focus();
+            //txtName.SelectAll();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult x;
+            x=MessageBox.Show("آیا می خواهید از برنامه خارج شوید؟?","خروچ.....",MessageBoxButtons.YesNo);
+            if (x == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void lblEnterYourName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEnterYourSalary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Text = string.Empty;
+            txtSalary.Text = string.Empty;
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSalary_TextChanged(object sender, EventArgs e)
+        {
+            btnClear.Enabled = Convert.ToBoolean(txtSalary.Text.Length);
+            btnCalculate.Enabled=Convert.ToBoolean(txtSalary.Text.Length);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSalary_TextChanged(null,null);
+        }
+    }
+}
